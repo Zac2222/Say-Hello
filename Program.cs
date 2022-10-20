7e3a5983d246c58a83ecf133b0875d78f26a6153
zac2222 <zacrice2222@gmail.com>
2022-10-19 20:09:07 -0700

    New say hello project with looping

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿bool runAgain = true;
+
+while (runAgain == true)
+{
+    string choice = "";
+    string name;
+    Console.WriteLine("What is your name?");
+    name = Console.ReadLine();
+    Console.WriteLine($"Hello, {name}!");
+    Console.WriteLine("run again? (y / n)");
+
+    while(choice != "yes" & choice != "y" & choice != "no" & choice != "n")
+    {
+        choice = Console.ReadLine().ToLower();
+    }
+    
+    if(choice == "y" || choice == "yes")
+    {
+        runAgain = true;
+    }
+    else if(choice == "n" || choice == "no")
+    {
+        runAgain = false;
+        Console.WriteLine("Now exiting");
+    }
+}
